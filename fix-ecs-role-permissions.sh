fb04f5635602b90f33b92c69089571552fee3e78
Pursharthzutshi <pursharthzutshi@gmail.com>
2025-10-09 01:48:08 +0530

    fix deployment

---
+++ fix-ecs-role-permissions.sh
@@ -0,0 +1,16 @@
+aws iam put-role-policy \
+  --role-name ecsTaskExecutionRole \
+  --policy-name CloudWatchLogsPolicy \
+  --policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    }]
+  }'
